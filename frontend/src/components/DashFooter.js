351a0afe8e609be026b9060256d3457dbef5bfa8
Julia Ajpert <jajpert@email.com>
2024-05-21 12:03:04 -0400

    commit inicial

---
+++ frontend/src/components/DashFooter.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate, useLocation } from 'react-router-dom'
+
+const DashFooter = () => {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+
+  const content = (
+    <footer className='dash-footer'>
+      <p>Current User:</p>
+      <p>Status:</p>
+    </footer>
+  )
+
+  return content
+}
+
+export default DashFooter
